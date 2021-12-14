0c3e39e620781a0d621acd19f53d04427615dedf
Ayisha <ayishetuseidu13@gmail.com>
2021-12-14 20:46:39 +0000

    arrange components on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import styled from '@emotion/styled';
-import {PageContainer} from './Styles'
+//import {PageContainer} from './Styles'
 import {Linkedin, GitHub, Mail} from 'react-feather'
 
 function Home() {
     return (
-        <PageContainer>
-            <About>
-            <Introduction>Hello, my name is Ayisha, a frontend developer with speacilisaion in reacct js</Introduction>
+        <Container>
+
+            <Introduction>
+           <h1>Ayisha </h1>
+           <h1>Seidu</h1>
+            <p>I am  Ghanaian frontend developer with speacilisaion in react js</p>
+            </Introduction>
            
             <Photo src='ayisha.jpg'/>
-            </About>
-
         <HomeFooter>
             <ContactLink href='https://www.linkedin.com/in/ayishetu-seidu-70431392/'>
             <Linkedin/> <br/>
@@ -28,27 +30,40 @@ function Home() {
             </ContactLink>
         </HomeFooter>
 
-        </PageContainer>
+        </Container>
     )
 }
 
-export default Home
+export default Home;
+
+const Container = styled.div`
+grid-area: content;
+display: grid;
+grid-template-column: repeat(2, 1fr);
+grid-template-row: 1fr 10rem;
+grid-template-areas: 
+'intro photo'
+'footer footer';
+background-color: lightviolet;
+`;
+
+const Introduction = styled.div`
+grid-area: intro;
+width: 50vw;
+font-size: 1rem;
+align-content: centre;
+`;
 
 const Photo = styled.img`
+grid-area: photo;
 width: auto;
 height: 20rem;
 border-radius: 1rem;
 `;
 
-const Introduction = styled.div``;
-
-const About = styled.div`
-display: flex;
-flex-direction: row;
-justify content: space-around;
-`;
 
 const HomeFooter = styled.div`
+grid-area: footer;
 display: flex;
 flex-direction: row;
 margin: auto;

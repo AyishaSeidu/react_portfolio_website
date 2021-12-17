fdaf6488742799e0d0e5062200c83b2255ad514e
Ayisha <ayishetuseidu13@gmail.com>
2021-12-17 19:48:34 +0000

    remove navbar

--- src/components/Site.js
+++ src/components/Site.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "@emotion/styled";
-import Nav from "./Nav";
 import Home, {} from './Home'
 import Projects from "./Projects";
 import Skills from "./Skills";
@@ -14,7 +13,6 @@ function Site() {
 
   return (
     <PageWrapper >
-      <Nav/>
       {/* <Home/> */}
       <Skills skills={skills}/>
     </PageWrapper>
@@ -26,13 +24,6 @@ export default Site;
 
 
 const PageWrapper = styled.div`
-display: grid;
-grid-template-rows: 3rem 1fr;
-grid-gap: 1rem;
-grid-template-areas: 
-'nav'
-'content'
-;
 font-size: 1rem;
 height: 100vh;
 width: 100vw;

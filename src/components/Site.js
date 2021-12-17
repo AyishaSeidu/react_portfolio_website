5ca25250333e5df784c03742b64ab7856d7d0844
Ayisha <ayishetuseidu13@gmail.com>
2021-12-17 19:53:43 +0000

    remove skills page

--- src/components/Site.js
+++ src/components/Site.js
@@ -2,19 +2,16 @@ import React from "react";
 import styled from "@emotion/styled";
 import Home, {} from './Home'
 import Projects from "./Projects";
-import Skills from "./Skills";
 
 
 function Site() {
 
   const projects = [{id: 0, name: 'Dashboard App', description: 'A React dashboard built with data from the JSONPlacehoder API', link: 'https://reverent-raman-1baaf8.netlify.app/', image: 'dashboard'} ,{id: 1, name: 'Todo App', description: 'Using react to built a single paged todo app', link: 'https://wonderful-brattain-c2056e.netlify.app/', image: 'todo'}];
 
-  const skills = ['react', 'nodejs', 'javascript', 'css', 'html', 'github'];
-
   return (
     <PageWrapper >
-      {/* <Home/> */}
-      <Skills skills={skills}/>
+      <Home/>
+      <Projects projects={projects}/>
     </PageWrapper>
   );
 

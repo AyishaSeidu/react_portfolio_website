1e89ca6243aa2ac1586d005602a62ce59886dcb9
Ayisha <ayishetuseidu13@gmail.com>
2022-01-16 12:58:16 +0000

    style page footer

--- src/components/Site.js
+++ src/components/Site.js
@@ -8,7 +8,7 @@ import Projects from "./Projects";
 
 function Site() {
 
-  const projects = [{id: 0, name: 'Dashboard App', description: 'A React dashboard built with data from the JSONPlacehoder API', link: 'https://reverent-raman-1baaf8.netlify.app/', image: 'dashboard'} ,{id: 1, name: 'Todo App', description: 'Using react to built a single paged todo app', link: 'https://wonderful-brattain-c2056e.netlify.app/', image: 'todo'}];
+  const projects = [{id: 0, name: 'Dashboard App', description: 'A React dashboard built with data from the JSONPlacehoder API', link: 'https://reverent-raman-1baaf8.netlify.app/', image: 'dashboard'} ,{id: 1, name: 'Todo App', description: 'A single paged todo app with react', link: 'https://wonderful-brattain-c2056e.netlify.app/', image: 'todo'}];
 
   return (
     <PageWrapper >
@@ -18,9 +18,9 @@ function Site() {
       </div>
       <>
       <Footer>
-        <Linkedin/>
-        <GitHub/>
-        <Mail/>
+       <FooterItem href="https://www.linkedin.com/in/ayishetu-seidu-70431392/" target="__blank"> <Linkedin size={30}/> </FooterItem> 
+       <FooterItem href="https://github.com/AyishaSeidu" target="__blank"> <GitHub size={30} /> </FooterItem>
+       <FooterItem> <Mail size={30}/> </FooterItem>
       </Footer>
       </>
     </PageWrapper>
@@ -48,10 +48,23 @@ grid-area: footer;
 display: flex;
 flex-direction: row;
 margin: auto;
-background-color: crimson;
+background-image: url("wave.svg");
+background-repeat: no-repeat;
+background-position: center;
+background-size: cover;
 padding: auto;
 width: 100vw;
 height: 100%;
 justify-content: space-around;
+align-items: flex-end;
 `;
 
+const FooterItem = styled.a`
+cursor: pointer;
+color: black;
+margin: auto;
+padding: 0.2rem;
+:hover {
+  color: tomato;
+}
+`;

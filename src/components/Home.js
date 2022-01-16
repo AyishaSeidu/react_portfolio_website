e1f1e5e8c9b53f6bb321160bf84fed8583992cf0
Ayisha <ayishetuseidu13@gmail.com>
2022-01-16 13:59:47 +0000

    add slidein animation to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from '@emotion/styled';
+import { keyframes } from '@emotion/react';
 //import {PageContainer} from './Styles'
 
 function Home() {
@@ -35,11 +36,39 @@ height: 100vh;
 width: 100vw;
 `;
 
+const PhotoAnimation = keyframes`
+0% {
+    margin-right: -25rem;
+}
+
+25% {
+    margin-right: 0rem;
+}
+
+100% {
+    margin-right: auto;
+}
+`;
+
+const IntroductionAnitation = keyframes`
+0% {
+    margin-left: -25rem;
+}
+
+25% {
+    margin-left: 0rem;
+}
+
+100% {
+    margin-left: none;
+}
+`;
+
 const Introduction = styled.div`
 width: 50vw;
 font-size: 1rem;
 align-content: center;
-//margin: auto;
+animation: ${IntroductionAnitation} 5s ease-in;
 `;
 
 const Name = styled.div`
@@ -68,14 +97,10 @@ backgound-position: center;
     width: 100vw;
     bottom: 1rem;
 }
+
+animation: ${PhotoAnimation} 5s ease-in;
 `;
 
-const Photo = styled.img`
-@media (max-width: 800px) {
-    height: 100%;
-    width: auto;
-    margin: 0rem auto auto auto;
-}
-`;
+
 
 
